5f5c7f60933929aa10936afb901957aa87f92b2a
Bram <bramstrik@live.com>
2025-02-04 14:25:01 +0100

    Fix unit tests

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,22 +1,60 @@
+import { Ship } from './ship'
+
 class GameBoard {
-  constructor (ships) {
-    this.ships = ships
-    this.board = new Array(10)
-      .fill(null)
-      .map(new Array(10))
-      .fill(null)
+  constructor (...shipSizes) {
+    const boardSize = 10
+    this.ships = shipSizes.map(size => new Ship(size))
+    this.attackBoard = new Array(boardSize)
+    this.shipBoard = new Array(boardSize)
+
+    for (let i = 0; i < this.attackBoard.length; i++) {
+      this.attackBoard[i] = new Array(boardSize).fill(false)
+      this.shipBoard[i] = new Array(boardSize).fill(false)
+    }
   }
 
-  receiveAttack () {
+  allSunk = () => this.ships.every(ship => ship.isSunk())
 
+  isOnBoard (x, y) {
+    const isXOnBoard = x >= 0 && x < this.attackBoard.length
+    const isYOnBoard = y >= 0 && y < this.attackBoard.length
+    return isXOnBoard && isYOnBoard
   }
 
-  placeShipAt () {
-
+  // returns the ship on (x, y), or null otherwise
+  getShip (x, y) {
+    return this.shipBoard[y][x] === false ? null : this.shipBoard[y][x]
   }
 
-  allSunk () {
+  // Return true on success, false on failure
+  receiveAttack (x, y) {
+    if (!this.isOnBoard(x, y)) return false // invalid square
+    if (this.attackBoard[x][y]) return false // square has already been shot at
 
+    this.attackBoard[y][x] = true
+    this.getShip(x, y)?.hit()
+
+    return true
+  }
+
+  placeShipAt (x, y, direction, shipIndex) {
+    if (!this.isOnBoard(x, y)) return false
+
+    let length = this.ships[shipIndex].length - 1
+
+    if (direction === 'vertical') {
+      do {
+        this.shipBoard[y + length][x] = shipIndex
+        length--
+      } while (length >= 0)
+    } else if (direction === 'horizontal') {
+      do {
+        this.shipBoard[y][x + length] = shipIndex
+        length--
+      } while (length >= 0)
+    }
+
+    return true
   }
 }
 
