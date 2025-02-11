5c5bda1347677c50e93c1e6717b1b0c2167026d8
Bram <bramstrik@live.com>
2025-02-11 10:15:57 +0100

    Add Player class

---
+++ src/player.js
@@ -0,0 +1,13 @@
+import { GameBoard } from "./gameBoard"
+
+class Player {
+  constructor (options = {}) {
+    const startShips = [2, 2, 2, 2, 3, 3, 3, 4, 4, 5]
+    this.gameBoard = new GameBoard(...startShips)
+    this._cpu = options?.cpu
+  }
+
+  isHuman = () => !this._cpu
+}
+
+export { Player }
