ee8a05e443423dbe1dfa1c259966f200c51aa48b
Bram <bramstrik@live.com>
2025-02-11 13:00:15 +0100

    Add normalize.css and move style.css

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,14 @@
 'use-strict'
 
+// CSS imports
+import '../css/style.css'
+import 'normalize.css'
+
+// JS modules
 import { Ship } from './modules/ship'
 import { GameBoard } from './modules/gameBoard'
 import { Player } from './modules/player'
 import { GameLogic } from './modules/gameLogic'
 
+
+// Actual code goes here
